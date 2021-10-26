1731fe81601d31c063fa7d9e5502c5d1c8260e64
PlumloLee <73246048+PlumloLee@users.noreply.github.com>
2021-10-26 02:23:56 -0700

    Update main.py

--- main.py
+++ main.py
@@ -184,15 +184,14 @@ class Zlapp(Fudan):
         geo_api_info = json_loads(self.last_info["geo_api_info"])
         province = self.last_info["province"]
         city = self.last_info["city"]
-	
-	xs_sfdyz = self.last_info["xs_sfdyz"]
-	xs_dyzdd = self.last_info["xs_dyzdd"]
-	xs_dyzdd_text = self.last_info["xs_dyzdd_text"]
-	xs_sfdez = self.last_info["xs_sfdez"]
-	xs_dezdd = self.last_info["xs_dezdd"]
-	xs_dezdd_text = self.last_info["xs_dezdd_text"]
-	district = geo_api_info["addressComponent"].get("district", "")
-	self.last_info.update()
+        xs_sfdyz = self.last_info["xs_sfdyz"]
+        xs_dyzdd = self.last_info["xs_dyzdd"]
+        xs_dyzdd_text = self.last_info["xs_dyzdd_text"]
+        xs_sfdez = self.last_info["xs_sfdez"]
+        xs_dezdd = self.last_info["xs_dezdd"]
+        xs_dezdd_text = self.last_info["xs_dezdd_text"]
+        district = geo_api_info["addressComponent"].get("district", "")
+        self.last_info.update()
 
         
         while(True):
